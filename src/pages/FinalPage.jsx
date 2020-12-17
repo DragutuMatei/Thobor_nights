27aa765331179f999ee43c8317200660d172fcce
DragutuMatei <67979189+DragutuMatei@users.noreply.github.com>
2020-12-17 19:36:47 +0200

    asd

--- src/pages/FinalPage.jsx
+++ src/pages/FinalPage.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Back from "../components/Back";
+import { init } from "emailjs-com";
+import emailjs from "emailjs-com";
 
 function FinalPage() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [experienta, setExperienta] = useState("");
+
+  init("user_F1FXOrwoiGSWERQpZLjlR");
+
+  const submit = () => {
+    const serviceID = "service_0mpw36j";
+    const templateID = "template_dd1upta";
+    const templateParams = {
+      name: name,
+      email: email,
+      experinta: experienta,
+    }; 
+
+    emailjs.send(serviceID, templateID, templateParams).then(
+      function (response) {
+        console.log("SUCCESS!", response.status, response.text);
+      },
+      function (error) {
+        console.log("FAILED...", error);
+      }
+    );
+  };
+
   return (
     <>
       {window.sessionStorage.getItem("Bilet") && (
@@ -10,12 +37,51 @@ function FinalPage() {
             className="fullPage l"
             style={{
               display: "flex",
-              justifyContent: "space-evenly",
+              justifyContent: "flex-start",
               flexDirection: "column",
               padding: "0 100px",
             }}
           >
             <Back />
+            <div className="titlu">
+              <h1>Va multumim pentru participare</h1>
+            </div>
+            <form className="form">
+              <div className="left">
+                <div className="row">
+                  <input
+                    required
+                    type="text"
+                    placeholder="Numele"
+                    onChange={(e) => {
+                      setName(e.target.value);
+                    }}
+                  />
+                </div>
+                <div className="row">
+                  <input
+                    required
+                    type="email"
+                    placeholder="Email"
+                    onChange={(e) => {
+                      setEmail(e.target.value);
+                    }}
+                  />
+                </div>
+                <div className="row">
+                  <button onClick={submit}>Submit</button>
+                </div>
+              </div>
+              <div className="right">
+                <textarea
+                  required
+                  onChange={(e) => {
+                    setExperienta(e.target.value);
+                  }}
+                  placeholder="Cum ti s-a parut aceasta experienta?"
+                ></textarea>
+              </div>
+            </form>
           </div>
         </>
       )}
